7ea7adfe1163c2ae99735445202d9c574a86d91b
Max <108975840+poeminthewind@users.noreply.github.com>
2025-08-08 15:50:06 +0800

    Update campaign.utils.ts

--- utils/campaign.utils.ts
+++ utils/campaign.utils.ts
@@ -17,10 +17,12 @@ export class CampaignUtils {
 
         const isEcoFriendExists = await this.page.getByRole('cell', { name: ' Eco friendly' }).isVisible();
         if(!isEcoFriendExists) {
-            await this.page.getByRole('button', { name: ' New campaign' }).click();
-            await this.page.getByRole('cell', { name: 'Eco-friendly Increases' }).click();
-            await this.page.waitForSelector('role=button[name="$"]', { timeout: 10000 });
-            await this.page.click('role=button[name="$"]');
+            await this.page.getByRole('button', { name: /New campaign/i }).click();
+            await this.page.getByRole('cell', { name: /Eco-friendly Increases/i }).click();
+            await this.page.waitForLoadState('domcontentloaded');
+            const dollarButton = this.page.getByRole('button', { name: /\$/ });
+            await dollarButton.waitFor({ timeout: 30000 });
+            await dollarButton.click();
             console.log("Eco Friendly Campaign Created Successfully!");
         }
 
